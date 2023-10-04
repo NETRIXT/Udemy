240b85ce08a4cf015de23455ca3fa60f7fbe210c
Vladislav Suvalov <vladislav.suvalov@hp.com>
2023-10-04 16:51:05 +0300

    add: new state, submit

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,16 +2,19 @@ import { useState } from "react";
 import "./ExpenseForm.css";
 
 const ExpenseForm = () => {
-  // const [enteredTitle, setEnteredTitle] = useState("");
-  // const [enteredNumber, setEnteredNumber] = useState("");
-  // const [enteredDate, setEnteredDate] = useState("");
-  const [userInput, setUserInput] = useState({
-    enteredTitle: "",
-    setEnteredNumber: "",
-    enteredDate: "",
-  });
+  const [enteredTitle, setEnteredTitle] = useState("");
+  const [enteredNumber, setEnteredAmount] = useState("");
+  const [enteredDate, setEnteredDate] = useState("");
+  // const [userInput, setUserInput] = useState({
+  //   enteredTitle: "",
+  //   setEnteredNumber: "",
+  //   enteredDate: "",
+  // });
+
+
 
   const titleChangeHandler = (e) => {
+    setEnteredTitle(e.target.value);
     // setUserInput({
     //   ...userInput,
     //   enteredTitle: e.target.value,
@@ -22,21 +25,45 @@ const ExpenseForm = () => {
   };
 
   const nubmerChangeHandler = (e) => {
-    setUserInput({
-      ...userInput,
-      setEnteredNumber: e.target.value,
-    });
+    setEnteredAmount(e.target.value);
+    // setUserInput({
+    //   ...userInput,
+    //   setEnteredNumber: e.target.value,
+    // });
   };
 
   const dateChangeHandler = (e) => {
+    setEnteredDate(e.target.value);
     // setUserInput({
     //   ...userInput,
     //   enteredDate: e.target.value,
     // });
   };
 
+  // const inputChangeHandler = (identifier, value) => {
+  //   if (identifier === 'title') {
+  //     setEnteredTitle(value);
+  //   }
+  //   else if (identifier === 'date') {
+  //     setEnteredDate(value);
+  //   } else {
+  //     setEnteredAmount(value);
+  //   }
+  // };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const expenseData = {
+      title: enteredTitle,
+      amount: enteredNumber,
+      date: new Date(enteredDate)
+    };
+    console.log(expenseData);
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>

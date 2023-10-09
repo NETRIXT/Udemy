f2241861f12d4256c0821706a5d9600de474c8f8
Vladislav Suvalov <vladislav.suvalov@hp.com>
2023-10-09 15:47:21 +0300

    add new state

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -60,6 +60,9 @@ const ExpenseForm = () => {
       date: new Date(enteredDate)
     };
     console.log(expenseData);
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('')
   };
 
   return (
@@ -67,13 +70,14 @@ const ExpenseForm = () => {
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
         </div>
 
         <div className="new-expense__control">
           <label>Amount</label>
           <input
             type="number"
+            value={enteredNumber}
             onChange={nubmerChangeHandler}
             min="0.01"
             step="0.01"
@@ -84,6 +88,7 @@ const ExpenseForm = () => {
           <label>Date</label>
           <input
             type="date"
+            value={enteredDate}
             onChange={dateChangeHandler}
             min="2023-01-01"
             max="2024-12-31"

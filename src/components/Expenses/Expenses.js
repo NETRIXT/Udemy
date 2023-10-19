0ba27ac4cab3b584f7fb1a5bc9080a4c523975a1
Vladislav Suvalov <vladislav.suvalov@hp.com>
2023-10-19 17:14:41 +0300

    add new state

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,35 +1,39 @@
 import "./Expenses.css";
 import Card from "../UI/Card";
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "../NewExpense/ExpensesFilter";
 
 const Expenses = (props) => {
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      />
+    <div>
+      <ExpensesFilter />
+      <Card className="expenses">
+        <ExpenseItem
+          title={props.expenses[0].title}
+          amount={props.expenses[0].amount}
+          date={props.expenses[0].date}
+        />
 
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
+        <ExpenseItem
+          title={props.expenses[1].title}
+          amount={props.expenses[1].amount}
+          date={props.expenses[1].date}
+        />
 
-      <ExpenseItem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      />
+        <ExpenseItem
+          title={props.expenses[2].title}
+          amount={props.expenses[2].amount}
+          date={props.expenses[2].date}
+        />
 
-      <ExpenseItem
-        title={props.expenses[3].title}
-        amount={props.expenses[3].amount}
-        date={props.expenses[3].date}
-      />
-    </Card>
+        <ExpenseItem
+          title={props.expenses[3].title}
+          amount={props.expenses[3].amount}
+          date={props.expenses[3].date}
+        />
+      </Card>
+    </div>
   );
-}
+};
 
 export default Expenses;
---
+++ src/components/NewExpense/ExpensesFilter.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+import './ExpensesFilter.css';
+
+const ExpensesFilter = (props) => {
+  const [chosenDate, setChosenDate] = useState("");
+
+  const dateChangeHandler = (e) => {
+    const selectedData = {
+      date: e.target.value
+    };
+    setChosenDate(e.target.value);
+    // props.onSaveSelectedData(selectedData);
+
+    console.log(selectedData);
+  }
+
+  // useEffect(() => {
+  //   console.log(chosenDate);
+  // }, [chosenDate]);
+
+  return (
+    <div className='expenses-filter'>
+      <div className='expenses-filter__control'>
+        <label>Filter by year</label>
+        <select onChange={dateChangeHandler} value={chosenDate} >
+          <option value='2022'>2022</option>
+          <option value='2021'>2021</option>
+          <option value='2020'>2020</option>
+          <option value='2019'>2019</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default ExpensesFilter;
